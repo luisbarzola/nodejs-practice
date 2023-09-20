78663b89bf48eedc676478c1dbb2aa6ebc667c41
Luis Barzola <lrbarzola@gmail.com>
2023-09-20 19:30:07 -0300

    working with http

---
+++ 5.http.js
@@ -0,0 +1,16 @@
+const http = require('node:http')
+const { findEnablePort } = require('./6.free.port.js')
+
+const server = http.createServer((_, res) => {
+  console.log('request received')
+  res.end('hello word')
+});
+
+(async () => {
+  const desiredPort = await findEnablePort(3000)
+  const port = process.env.PORT ?? desiredPort
+
+  server.listen(port, () => {
+    console.log(`server listing on port: http://localhost:${server.address().port}`)
+  })
+})()
